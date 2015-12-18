4d633032fcb951f09cd59b23ad600f26cda827e4
Matthew Eichler <matthew.eichler@aventinesolutions.nl>
2015-12-18 16:37:18 +0100

    :bear: configured SAML
    ... for Rails and Ruby SAML

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,7 +4,7 @@ class Account < ActiveRecord::Base
     # should retrieve SAML-settings based on subdomain, IP-address, NameID or similar
     settings = OneLogin::RubySaml::Settings.new
 
-    url_base ||= "http://localhost:3000"
+    url_base ||= "http://localhost:3003"
 
     # Example settings data, replace this values!
 
@@ -17,25 +17,36 @@ class Account < ActiveRecord::Base
     settings.assertion_consumer_logout_service_url = url_base + "/saml/logout"
 
     # IdP section
-    settings.idp_entity_id                  = "https://app.onelogin.com/saml/metadata/<onelogin-app-id>"
-    settings.idp_sso_target_url             = "https://app.onelogin.com/trust/saml2/http-post/sso/<onelogin-app-id>"
-    settings.idp_slo_target_url             = "https://app.onelogin.com/trust/saml2/http-redirect/slo/<onelogin-app-id>"
-    settings.idp_cert                       = "-----BEGIN CERTIFICATE-----
-MIICbjCCAdegAwIBAgIBADANBgkqhkiG9w0BAQ0FADBUMQswCQYDVQQGEwJ1czET
-MBEGA1UECAwKQ2FsaWZvcm5pYTEVMBMGA1UECgwMT25lbG9naW4gSW5jMRkwFwYD
-VQQDDBBhcHAub25lbG9naW4uY29tMB4XDTE0MDkxMTE1MDUxMVoXDTE1MDkxMTE1
-MDUxMVowVDELMAkGA1UEBhMCdXMxEzARBgNVBAgMCkNhbGlmb3JuaWExFTATBgNV
-BAoMDE9uZWxvZ2luIEluYzEZMBcGA1UEAwwQYXBwLm9uZWxvZ2luLmNvbTCBnzAN
-BgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAsezUGLSiNMXg80EZMMzvXH43f07a1plU
-mm1poYvVfgSICTqhEUuA0x4w9w/K4BegO07GVkUjNCrvtJEqq4FMDbHj2VfOCMHx
-lYi52/ELXKe6ALSm48y5BG9fd1kGHBqBg741KpMvDkmAbX1sLq5reAjOccIDme2d
-lLD8tQ8y0IMCAwEAAaNQME4wHQYDVR0OBBYEFP3cLGEyby7TBXweK0SeFrvnRoHL
-MB8GA1UdIwQYMBaAFP3cLGEyby7TBXweK0SeFrvnRoHLMAwGA1UdEwQFMAMBAf8w
-DQYJKoZIhvcNAQENBQADgYEAS3vJKRFGjvxOHHNJR77wYlxSSbyb9vmWOVBzaTMd
-5TFfoSDxuP9RskRjSrez/63WON6tdYr/mJNSNB70ZAic824Y9feQ/kBaCswI+Pgp
-b6sFIh4CrY4yCEkBPD8jXFCeJMq+2AqshbITpiu7WF1RCAp/sRAO56giP/B4l0o0
-Tc0=
------END CERTIFICATE-----"
+    settings.idp_entity_id                  = 'https://app.onelogin.com/saml/metadata/501136'
+    settings.idp_sso_target_url             = 'https://reelmetrics.onelogin.com/trust/saml2/http-post/sso/501136'
+    settings.idp_slo_target_url             = 'https://reelmetrics.onelogin.com/trust/saml2/http-redirect/slo/501136'
+    settings.idp_cert                       =
+'    -----BEGIN CERTIFICATE-----
+MIIEIDCCAwigAwIBAgIURkAf7e/p6iWHlheRmP0ThvoUQpgwDQYJKoZIhvcNAQEF
+BQAwWzELMAkGA1UEBhMCVVMxFDASBgNVBAoMC1JlZWxNZXRyaWNzMRUwEwYDVQQL
+DAxPbmVMb2dpbiBJZFAxHzAdBgNVBAMMFk9uZUxvZ2luIEFjY291bnQgNzI3ODgw
+HhcNMTUxMTIzMTYwNTQ4WhcNMjAxMTI0MTYwNTQ4WjBbMQswCQYDVQQGEwJVUzEU
+MBIGA1UECgwLUmVlbE1ldHJpY3MxFTATBgNVBAsMDE9uZUxvZ2luIElkUDEfMB0G
+A1UEAwwWT25lTG9naW4gQWNjb3VudCA3Mjc4ODCCASIwDQYJKoZIhvcNAQEBBQAD
+ggEPADCCAQoCggEBAMhLnDz5sHVi3prLRowTHOrreIjvrDLZg71zX7gcvtk15jW9
+bPyEYazODO1dAgtxaOXNUvVTfI6IleEPFfqiiRZD9aNFx6t1SWbZWbTO3NRzQZnD
++MuB9r3toPLDJecKFUa86vGGLSrPaZukTjcU7S6bHC3swXf+hvHU02H5KykwKQDJ
+fsLycJrdaVhy2wiiKwJNuMpmxTB3rzqJe20mcsC902PbZjsscc0YEWxGxQfFiyLj
+dTeb2pepBBSxUGrBuR1X1KUPFZSTTTJZYc1PLqpAE1KSIMildGzr0vyQbtXk19oZ
+Vt9xDDtIXrX5LMOgk6X9Hrctzm41IukMoneB1j8CAwEAAaOB2zCB2DAMBgNVHRMB
+Af8EAjAAMB0GA1UdDgQWBBRTzVAIIYYRdeX6lwt48GxxRns2XDCBmAYDVR0jBIGQ
+MIGNgBRTzVAIIYYRdeX6lwt48GxxRns2XKFfpF0wWzELMAkGA1UEBhMCVVMxFDAS
+BgNVBAoMC1JlZWxNZXRyaWNzMRUwEwYDVQQLDAxPbmVMb2dpbiBJZFAxHzAdBgNV
+BAMMFk9uZUxvZ2luIEFjY291bnQgNzI3ODiCFEZAH+3v6eolh5YXkZj9E4b6FEKY
+MA4GA1UdDwEB/wQEAwIHgDANBgkqhkiG9w0BAQUFAAOCAQEApKl0AbKWIBws7O+Z
+hL1c6qxBwwmKSV9jCP9TGmO95yOdJwgrEflZg648kWivvosCFXY6xy1gFhpTIlla
+2Gsr5OJ/F3T2eN6Qz1JFbKIqDvK2y7W6IC4b6AhteuckZxG1qUxYXzhHx+Y2YiC6
+RTm2BX+n0w4L6AbdhUNDMcgH+ur7S+GrZPPuqU1VBfBaRel5M5RgIwdupuflnPJn
+//X53hfEnJohiEskxS2W23RPBMeqSY5FpRc+twp8AdD4gLpbtJX6RdQfa1lz/t30
+ixTsMC+pQ2My5Ekn+lNypmFs9SSQiPG8ZSyoLn+3PoeHlAVDBE5h7w8nSbOd1yPQ
+VyhqJg==
+    -----END CERTIFICATE-----'
+
     # or settings.idp_cert_fingerprint           = "3B:05:BE:0A:EC:84:CC:D4:75:97:B3:A2:22:AC:56:21:44:EF:59:E6"
     #    settings.idp_cert_fingerprint_algorithm = XMLSecurity::Document::SHA1
 
